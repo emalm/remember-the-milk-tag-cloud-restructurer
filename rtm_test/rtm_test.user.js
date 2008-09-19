07ebc80bed71309c7a94b427775da0d4e0c68e5f
Eric Malm <eric.malm@gmail.com>
2008-09-19 16:40:42 +0000

    Added files for Remember the Milk javascript project.

---
+++ rtm_test/rtm_test.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           RTM test
+// @namespace      rtm_test
+// @include        http://www.rememberthemilk.com/*
+// @include        https://www.rememberthemilk.com/*
+// @include        http://*.www.rememberthemilk.com/*
+// @include        https://*.www.rememberthemilk.com/*
+// ==/UserScript==
